fca1a08034d7b69e47a9eb7f74b51ef6a073294c
Omar Akkulov <omar@payzen.online>
2021-12-07 20:26:46 +0300

    Осуществляем проверку токена с помощью внедрения значения из конфиг файла

--- src/main/java/ru/akkulov/paysonixspringboot/config/BeanConfig.java
+++ src/main/java/ru/akkulov/paysonixspringboot/config/BeanConfig.java
@@ -2,8 +2,10 @@ package ru.akkulov.paysonixspringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:myApp.properties")
 public class BeanConfig {
 
     @Bean
