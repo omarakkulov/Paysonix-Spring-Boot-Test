b37cfdff5ac801a8233af304990494416fd31722
Omar Akkulov <omar@payzen.online>
2021-12-07 20:01:11 +0300

    Реализуем проверку токена с помощью Spring Interceptor, заменив реализацию с использованием фильтров

--- src/main/java/ru/akkulov/paysonixspringboot/config/WebConfig.java
+++ src/main/java/ru/akkulov/paysonixspringboot/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor);
+        registry.addInterceptor(interceptor).addPathPatterns("/api/hmac/**");
     }
 }
--- src/main/java/ru/akkulov/paysonixspringboot/exception/handle/GlobalExceptionHandler.java
+++ src/main/java/ru/akkulov/paysonixspringboot/exception/handle/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.akkulov.paysonixspringboot.exception.BadRequestException;
+import ru.akkulov.paysonixspringboot.exception.TokenIncorrectHeaderException;
 import ru.akkulov.paysonixspringboot.exception.objects.BadRequestDataObject;
 
 @ControllerAdvice
@@ -18,6 +19,17 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
+
+
+    @ExceptionHandler
+    public ResponseEntity<BadRequestDataObject> handleException(
+            TokenIncorrectHeaderException exception) {
+
+        BadRequestDataObject data = new BadRequestDataObject(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.FORBIDDEN);
+    }
+
 
     @ExceptionHandler
     public ResponseEntity<BadRequestDataObject> handleException(Exception exception) {
